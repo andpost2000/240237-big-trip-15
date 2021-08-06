0f4f1385962292b80079ceec4fbe9c920e488b83
andpost200 <andrey.chepulis@kreo.net>
2021-08-06 19:01:53 +0300

    render elements

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import {createAddNewPointTemplate} from './view/add-new-point.js';
+import {createEventsListTemplate} from './view/events-list';
+import {createEditPointTemplate} from './view/edit-point';
+import {createEventTemplate} from './view/event';
+import {createMenuTemplate} from './view/menu';
+import {createFiltersTemplate} from './view/filters';
+import {createSortTemplate} from './view/sort';
+import {createTripInfoTemplate} from './view/trip-info';
+import {createTripInfoMainTemplate} from './view/trip-info-main';
+import {createTripInfoCostTemplate} from './view/trip-info-cost';
+
+const POINT_COUNT = 3;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripControlsElement = document.querySelector('.trip-controls');
+render(tripControlsElement, createTripInfoTemplate(), 'beforeBegin');
+
+const tripInfoElement = document.querySelector('.trip-info');
+render(tripInfoElement, createTripInfoMainTemplate(), 'beforeend');
+render(tripInfoElement, createTripInfoCostTemplate(), 'beforeend');
+
+const tripMainNavigationElement = tripControlsElement.querySelector('.trip-controls__navigation');
+render(tripMainNavigationElement, createMenuTemplate(), 'beforeend');
+
+const tripFiltersElement = tripControlsElement.querySelector('.trip-controls__filters');
+render(tripFiltersElement, createFiltersTemplate(), 'beforeend');
+
+const tripEventsElement = document.querySelector('.trip-events');
+render(tripEventsElement, createSortTemplate(), 'beforeend');
+render(tripEventsElement, createEventsListTemplate(), 'beforeend');
+
+const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
+render(tripEventsListElement, createEditPointTemplate(), 'beforeend');
+for (let i = 0; i < POINT_COUNT; i++) {
+  render(tripEventsListElement, createEventTemplate(), 'beforeend');
+}
+render(tripEventsListElement, createAddNewPointTemplate(), 'beforeend');
