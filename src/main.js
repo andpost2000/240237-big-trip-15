ffbef3626f57a637f75299093e553bbf9dcc7579
andpost200 <andrey.chepulis@kreo.net>
2021-08-10 16:43:25 +0300

    total cost

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-import {createAddNewPointTemplate} from './view/add-new-point.js';
-import {createEventsListTemplate} from './view/events-list';
-import {createEditPointTemplate} from './view/edit-point';
-import {createEventTemplate} from './view/event';
-import {createMenuTemplate} from './view/menu';
-import {createFiltersTemplate} from './view/filters';
-import {createSortTemplate} from './view/sort';
-import {createTripInfoTemplate} from './view/trip-info';
-import {createTripInfoMainTemplate} from './view/trip-info-main';
-import {createTripInfoCostTemplate} from './view/trip-info-cost';
-import {generatePoint} from './mock/point';
+import { createAddNewPointTemplate } from './view/add-new-point.js';
+import { createEventsListTemplate } from './view/events-list';
+import { createEditPointTemplate } from './view/edit-point';
+import { createEventTemplate } from './view/event';
+import { createMenuTemplate } from './view/menu';
+import { createFiltersTemplate } from './view/filters';
+import { createSortTemplate } from './view/sort';
+import { createTripInfoTemplate } from './view/trip-info';
+import { createTripInfoMainTemplate } from './view/trip-info-main';
+import { createTripInfoCostTemplate } from './view/trip-info-cost';
+import { generatePoint } from './mock/point';
 
-const POINT_COUNT = 7;
+const POINT_COUNT = 15;
+const POINTS_DATA = [];
+
+const generatePoints = (count) => {
+  for (let i = 0; i < count; i++) {
+    const point = generatePoint();
+    POINTS_DATA.push(point);
+  }
+};
+
+generatePoints(POINT_COUNT);
 
 
 const render = (container, template, place) => {
@@ -22,7 +32,7 @@ render(tripControlsElement, createTripInfoTemplate(), 'beforeBegin');
 
 const tripInfoElement = document.querySelector('.trip-info');
 render(tripInfoElement, createTripInfoMainTemplate(), 'beforeend');
-render(tripInfoElement, createTripInfoCostTemplate(), 'beforeend');
+render(tripInfoElement, createTripInfoCostTemplate(POINTS_DATA), 'beforeend');
 
 const tripMainNavigationElement = tripControlsElement.querySelector('.trip-controls__navigation');
 render(tripMainNavigationElement, createMenuTemplate(), 'beforeend');
@@ -36,8 +46,5 @@ render(tripEventsElement, createEventsListTemplate(), 'beforeend');
 
 const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
 render(tripEventsListElement, createEditPointTemplate(generatePoint()), 'beforeend');
-for (let i = 0; i < POINT_COUNT; i++) {
-  const point = generatePoint();
-  render(tripEventsListElement, createEventTemplate(point), 'beforeend');
-}
+POINTS_DATA.forEach((point) => render(tripEventsListElement, createEventTemplate(point), 'beforeend'));
 render(tripEventsListElement, createAddNewPointTemplate(), 'beforeend');
--- src/view/trip-info-cost.js
+++ src/view/trip-info-cost.js
@@ -1,5 +1,7 @@
-export const createTripInfoCostTemplate = () => (
-  `<p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-  </p>`
-);
+export const createTripInfoCostTemplate = (data) => {
+  let total = 0;
+  data.forEach((item) => total = total + item.cost);
+  return `<p class="trip-info__cost">
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${total}</span>
+  </p>`;
+};
